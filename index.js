6474b0b6b87285bad42f64e2655f5e5439ed14d7
Leon™ <leontm.dev@gmail.com>
2023-10-10 08:48:31 +0200

    First Commit

---
+++ index.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+// Imports
+
+import chalk from "chalk";
+import inquirer from "inquirer";
+import gradient from "gradient-string";
+import chalkAnimation from "chalk-animation";
+import figlet from "figlet";
+import { createSpinner } from "nanospinner";
+
+// Code
+
+console.log(chalk.bgBlue("Hallo"));
